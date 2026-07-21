518449d21e052a794d03159f55b1f961505132be
bradydisla <bradydue@gmail.com>
2026-07-20 20:20:04 -0400

    GO5

--- scripts/place.js
+++ scripts/place.js
@@ -22,14 +22,7 @@ const windSpeed = Number(
 const windChillElement = document.getElementById("wind-chill");
 
 // Calculate Wind Chill (Metric Formula)
-function calculateWindChill(temp, speed) {
-  return (
-    13.12 +
-    (0.6215 * temp) -
-    (11.37 * Math.pow(speed, 0.16)) +
-    (0.3965 * temp * Math.pow(speed, 0.16))
-  ).toFixed(1);
-}
+function calculateWindChill(temp, speed) { return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1); }
 
 // Display result only if the official conditions are met
 if (temperature <= 10 && windSpeed > 4.8) {
